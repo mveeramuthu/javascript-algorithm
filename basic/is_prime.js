2e5e730a13f2925054cc1ceb9d2c44c1381bb4c2
Mohanasundaram Veeramuthu <Mohan.Veeramuthu@iacpublishinglabs.com>
2016-07-14 01:44:21 -0700

    To verify whether given number is prime

---
+++ basic/is_prime.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Created by veeramuthum on 7/14/16.
+ */
+
+function isPrime(n) {
+    return true;
+}
+
+for(var i=0; i<=9; i++) {
+    if(isPrime(i)) {
+        console.log(i + " is prime");
+    } else {
+        console.log(i + " is not prime");
+    }
+}
